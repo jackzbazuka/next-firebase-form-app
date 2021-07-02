663fb7139de86167eab455bfbb64d5f979de64bf
ShivS01 <shivanshusingh10@yahoo.in>
2021-07-03 01:42:42 +0530

    extracting data from form and feeding to firebase works now partially

--- pages/api/firestore.js
+++ pages/api/firestore.js
@@ -1,32 +1,45 @@
-import admin from "@/firebase/adminApp"
-import firebase from 'firebase-admin'
+import admin from "@/firebase/adminApp";
+import firebase from "firebase-admin";
 
 export default async function handler(req, res) {
+  const { method, body } = req;
 
-	const { method, body } = req
+  const db = admin.firestore();
 
-	const db = admin.firestore()
+  if (method == "POST") {
+    await db
+      .collection(`${body.course}-${body.graduationYear}`)
+      .doc(body.firstName)
+      .set({
+        firstName: body.firstName,
+        lastName: body.lastName,
+        mobile: body.mobile,
+        linkedinurl: body.linkedinurl,
+        githuburl: body.githuburl,
+        email: body.email,
+        address: body.address,
+        age: body.age,
+        gender: body.gender,
+        course: body.course,
+        admissionYear: body.admissionYear,
+        graduationYear: body.graduationYear,
+        cgpa: body.cgpa,
+        tenPercentile: body.tenPercentile,
+        twelvePercentile: body.twelvePercentile,
+        programmingLanguage: body.programmingLanguage,
+        toolsAndTechnologies: body.toolsAndTechnologies,
+        coreSkills: body.coreSkills,
+        hobbies: body.hobbies,
+        certNcourse: body.certNcourse,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      });
 
-	if (method == 'POST') {
+    console.log(`${body.firstName} entered in firestore`);
 
-		await db.collection(body.course).doc(body.name).set({
-			name: body.name,
-			mobile: body.mobile,
-			email: body.email,
-			course: body.course,
-			createdAt: firebase.firestore.FieldValue.serverTimestamp()
-		})
-
-		console.log(`${body.name} entered in firestore`)
-
-		res.status(201).json({ message: 'User created succesfully' })
-
-	} else if (method == 'GET') {
-
-		res.status(200).json({ message: 'Data' })
-
-
-	} else {
-		res.status(405).json({ message: 'Method not allowed' })
-	}
+    res.status(201).json({ message: "User created succesfully" });
+  } else if (method == "GET") {
+    res.status(200).json({ message: "Data" });
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,62 +1,101 @@
-import { useState, useRef } from "react"
-import axios from "axios"
-import { storage } from "@/firebase/clientApp"
+import { useState, useRef } from "react";
+import axios from "axios";
+import { storage } from "@/firebase/clientApp";
 
-import PersonalDetails from "@/components/PersonalDetails"
-import Academics from "@/components/Academics"
-import Skills from "@/components/Skills"
-import Extracurricular from "@/components/Extracurricular"
-import Project from "@/components/Project"
-import Internship from "@/components/Internship"
-import Leadership from "@/components/Leadership"
+import PersonalDetails from "@/components/PersonalDetails";
+import Academics from "@/components/Academics";
+import Skills from "@/components/Skills";
+import Extracurricular from "@/components/Extracurricular";
+import Project from "@/components/Project";
+import Internship from "@/components/Internship";
+import Leadership from "@/components/Leadership";
 
 export default function Form() {
+  const [image, setImage] = useState(null);
 
-	const [image, setImage] = useState(null)
+  const imageRef = useRef(null);
 
-	const imageRef = useRef(null)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-	const handleSubmit = async (e) => {
-		e.preventDefault()
+    // console.log(e.target.elements);
+    const data = new FormData(e.target);
 
-		console.log(e.target.elements)
+    let student = {
+      // personal
+      firstName: data.get("firstName"),
+      lastName: data.get("lastName"),
+      mobile: data.get("mobile"),
+      linkedinurl: data.get("linkedinurl"),
+      githuburl: data.get("githuburl"),
+      email: data.get("email"),
+      address: data.get("address"),
+      age: data.get("age"),
+      gender: data.get("gender"),
+      // academics
+      course: data.get("course"),
+      admissionYear: data.get("admissionYear"),
+      graduationYear: data.get("graduationYear"),
+      cgpa: data.get("cgpa"),
+      tenPercentile: data.get("tenPercentile"),
+      twelvePercentile: data.get("twelvePercentile"),
+      // skills
+      programmingLanguage: data.get("programmingLanguage"),
+      toolsAndTechnologies: data.get("toolsAndTechnologies"),
+      coreSkills: data.get("coreSkills"),
+      // extracurricular
+      hobbies: data.get("hobbies"),
+      certNcourse: data.get("certNcourse"),
+      // internship
+    };
 
-		// const imgUpload = storage
-		// 	.ref(`images/${image.name}`)
-		// 	.put(image)
-		// 	.on("state_changed", null, (err) => console.log(err))
+    // console.log(student);
+    // const testImg = data.get("profilepicture");
+    // console.log(testImg);
+    // if (testImg)
+    //   storage
+    //     .ref(`images/${image.name}`)
+    //     .put(testImg)
+    //     .on("state_changed", null, (err) => console.log(err));
 
-		// const res = await axios({
-		// 	method: "POST",
-		// 	url: "/api/firestore",
-		// 	data: { name, mobile, email, course },
-		// })
+    const res = await axios({
+      method: "POST",
+      url: "/api/firestore",
+      data: student,
+    });
 
-		// if (res.status == 201) {
-		// 	setName("");
-		// 	setMobile("");
-		// 	setEmail("");
-		// 	setCourse("Btech");
-		// 	imageRef.current.value = null
-		// }
-	}
+    // if (res.status == 201) {
+    //   imageRef.current.value = null;
+    // }
+  };
 
-	return (
-		<div className='m-1 p-1 grid place-items-center'>
-			<h1 className='mx-auto mt-5 mb-1 p-1 text-xl'>STME Resume Form</h1>
-			<form className='mx-auto mt-1 mb-10 p-3 w-8/12 flex flex-col justify-around place-items-center'>
-				<PersonalDetails />
-				<Academics />
-				<Skills />
-				<Extracurricular />
-				<Internship />
-				<Project />
-				<Leadership />
-				<div className='m-1 p-1 w-full border-t-2 flex flex-col justify-around place-items-center '>
-					<p className='m-1 p-1 text-xs select-none italic'>(You've checked all the form values and are ready to submit the data)</p>
-					<button type='submit' className='mx-auto my-5 p-2 w-1/2 transition-all rounded-xl text-white bg-gray-700 hover:bg-gray-800'>Submit</button>
-				</div>
-			</form>
-		</div>
-	)
+  return (
+    <div className="m-1 p-1 grid place-items-center">
+      <h1 className="mx-auto mt-5 mb-1 p-1 text-xl">STME Resume Form</h1>
+      <form
+        className="mx-auto mt-1 mb-10 p-3 w-8/12 flex flex-col justify-around place-items-center"
+        onSubmitCapture={handleSubmit}
+      >
+        <PersonalDetails />
+        <Academics />
+        <Skills />
+        <Extracurricular />
+        <Internship />
+        <Project />
+        <Leadership />
+        <div className="m-1 p-1 w-full border-t-2 flex flex-col justify-around place-items-center ">
+          <p className="m-1 p-1 text-xs select-none italic">
+            (You've checked all the form values and are ready to submit the
+            data)
+          </p>
+          <button
+            type="submit"
+            className="mx-auto my-5 p-2 w-1/2 transition-all rounded-xl text-white bg-gray-700 hover:bg-gray-800"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
