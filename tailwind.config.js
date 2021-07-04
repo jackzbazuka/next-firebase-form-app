5d9ea2a637c7a9659a9204feb9f8baf3d58857a4
jackzbazuka <milindmoto99@gmail.com>
2021-07-04 11:35:34 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+	purge: ['./pages/**/*.{js}', './components/**/*.{jsx}'],
 	darkMode: false,
 	theme: {
 		extend: {},
