9ff668a13662d2f7efe5b252a2c31e2dd37ca745
jackzbazuka <milindmoto99@gmail.com>
2021-07-06 19:06:00 +0530

    implemented auth blocking and download feature

--- pages/form.js
+++ pages/form.js
@@ -18,11 +18,34 @@ export default function Form() {
 	const [leadCount, setLeadCount] = useState([])
 	const [projectCount, setProjectCount] = useState([])
 	const [internCounter, setInternCounter] = useState([])
+	const [sUser, setSUser] = useState(null)
+	const [hasData, setHasData] = useState(false)
+	const [userData, setUserData] = useState(null)
 	let dynamicCount = leadCount.length + projectCount.length + internCounter.length
 	// console.log(dynamicCount)
 	// console.log(leadCount.length + projectCount.length + internCounter.length)
-	const [sUser, setSUser] = useState(null)
-	const [hasData, setHasData] = useState(false)
+
+	useEffect(() => {
+		firebase.auth().onAuthStateChanged(async user => {
+			if (user) {
+				const myUserID = user.uid
+				const myForm = await firebase.firestore().collectionGroup('students').where('studentID', '==', myUserID).get()
+				setSUser(user)
+				await myForm.forEach(doc => {
+					if (doc.exists) {
+						let temp = doc.data()
+						delete temp.studentID
+						delete temp.createdAt
+						setUserData(temp)
+						setHasData(true)
+					}
+				})
+			} else {
+				router.push("/")
+			}
+		})
+	}, [])
+
 	const addLead = () => {
 		if (dynamicCount < 8) {
 			setLeadCount([...leadCount, leadCount.length])
@@ -38,30 +61,6 @@ export default function Form() {
 			setProjectCount([...projectCount, projectCount.length])
 		} else console.log("You've reached recommended limit of dynamic fields")
 	}
-
-	useEffect(() => {
-		firebase.auth().onAuthStateChanged(async (user) => {
-			if (
-				user
-				// && user.email.split("@")[1] === "nmims.edu.in"
-			) {
-				// console.log(user.email.split("@")[1])
-				setSUser(user)
-				console.log(user.uid, "Is logged in")
-			}
-			// else if (user
-			// 	 && user.email.split("@")[1] !== "nmims.edu.in"
-			// 	 ) {
-			// 	console.log("Please sign in with NMIMS ID")
-			// 	handleSignOut()
-			// }
-			else {
-				console.log("Is logged out")
-				router.push("/")
-			}
-		})
-	}, [])
-
 	const removeLead = () => {
 		let lead = [...leadCount]
 		lead.pop()
@@ -98,13 +97,8 @@ export default function Form() {
 		return monthYear
 	}
 
-	const handleDownload = async (e) => {
-		e.preventDefault()
-	}
-
 	const handleSignOut = async (e) => {
-		// e.preventDefault()
-
+		e.preventDefault()
 		firebase
 			.auth()
 			.signOut()
@@ -117,11 +111,10 @@ export default function Form() {
 	}
 
 	const handleSubmit = async (e) => {
+
 		e.preventDefault()
-		console.log("Submitted")
-		// console.log(e.target.elements);
+
 		const data = new FormData(e.target)
-		//console.log(data.getAll("desc"))
 
 		let leadershipArray = leadCount.map((leadership) => {
 			const leadershipData = {
@@ -132,7 +125,6 @@ export default function Form() {
 				)} - ${dateFormatter(data.get(`leadDuraEnd${leadership}`))}`,
 				leadDesc: data.getAll(`leadDesc${leadership}`),
 			}
-			// console.log('leadership data -->', leadershipData)
 			return leadershipData
 		})
 
@@ -145,7 +137,6 @@ export default function Form() {
 				)} - ${dateFormatter(data.get(`internDuraEnd${internship}`))}`,
 				internDesc: data.getAll(`internDesc${internship}`),
 			}
-			// console.log('internship data -->', internshipData)
 			return internshipData
 		})
 
@@ -158,7 +149,6 @@ export default function Form() {
 				)}`,
 				projDesc: data.getAll(`projDesc${project}`),
 			}
-			// console.log('project data -->', projectData)
 			return projectData
 		})
 		// console.log(dateFormatter("2017-9-22"))
@@ -166,13 +156,12 @@ export default function Form() {
 		const student = {
 			// uid
 			uid: sUser.uid,
-			googleEmail: sUser.email,
 			// personal
 			firstName: data.get("firstName"),
 			lastName: data.get("lastName"),
-			sapId: data.get("sapId"),
+			sapID: data.get("sapId"),
 			keywords: data.get("keywords"),
-			email: data.get("email"),
+			email: sUser.email,
 			mobile: data.get("mobile"),
 			linkedinUrl: data.get("linkedinUrl"),
 			githubUrl: data.get("githubUrl"),
@@ -198,37 +187,30 @@ export default function Form() {
 			projectArray,
 			leadershipArray,
 		}
-		// console.log(student)
 		// console.log(internshipArray, projectArray, leadershipArray)
 		const sImg = data.get("profilePicture")
 
-		if (sImg) {
-			storage
-				.ref(`${student.course}-${student.graduationYear}/${student.sapId} ${student.uid}`)
-				.put(sImg)
-				.on("state_changed", null, (err) => console.log(err))
-		}
-
-		// const res = await axios({
-		// 	method: "POST",
-		// 	url: "/api/test",
-		// 	data: {
-		// 		uid: sUser.uid,
-		// 		name: sUser.displayName,
-		// 		email: sUser.email,
-		// 		sapId: student.sapId,
-		// 	},
-		// })
-
 		const res = await axios({
 			method: "POST",
-			url: "/api/test",
+			url: "/api/firestore",
 			data: student,
 		})
 
-		// if (res.status == 201) {
-		// 	router.reload()
-		// }
+		if (res.status == 201) {
+			if (sImg) {
+				storage
+					.ref(`${student.course}-${student.graduationYear}/${student.sapId}`)
+					.put(sImg)
+					.on("state_changed", null, (err) => console.log(err))
+			}
+			router.reload()
+			console.log('User created')
+		} else if (res.status == 304) {
+			alert('Incorrect SAP ID, document already exists')
+			console.log('Document already exists')
+		} else {
+			router.reload()
+		}
 	}
 
 	return (
@@ -236,11 +218,11 @@ export default function Form() {
 			<header className="px-auto w-full bg-gray-900 text-white">
 				<nav className="w-full py-10 md:py-8 flex flex-row justify-center place-items-center">
 					<img
-						className="p-1 mx-2 md:mx-5 my-1 h-14 md:h-12 bg-white select-none"
+						className="p-1 mx-2 md:mx-5 my-1 h-10 md:h-11 lg:h-12 xl:h-14 bg-white select-none"
 						src="/logo.png"
 						alt="Logo of NMIMS"
 					/>
-					<h1 className="p-1 mx-2 md:mx-5 my-1 text-xl select-none">
+					<h1 className="p-1 mx-2 md:mx-5 my-1 text-base md:text-lg lg:text-xl select-none">
 						NMIMS - School of Technology Management & Engineering
 					</h1>
 				</nav>
@@ -248,20 +230,21 @@ export default function Form() {
 			{sUser ? (
 				<>
 					<div className="p-2 w-full flex flex-row justify-end">
-						<p className="m-1 px-3 py-1 rounded select-none border">
+						<p className="m-1 px-1 md:px-3 py-1 rounded select-none text-xs md:text-base border">
 							Logged in as {sUser.displayName}
 						</p>
 						{hasData && (
-							<button
-								onClick={handleDownload}
-								className="m-1 px-3 py-1 rounded select-none transition-all text-white bg-gray-700 lg:hover:bg-gray-800"
+							<a
+								className="m-1 px-1 md:px-3 py-1 rounded select-none text-xs md:text-base transition-all text-white bg-gray-700 lg:hover:bg-gray-800"
+								href={`data:text/json;charset=utf-8,${encodeURIComponent(JSON.stringify(userData, null, 4))}`}
+								download={`${userData.sapID}.json`}
 							>
 								Download data
-							</button>
+							</a>
 						)}
 						<button
 							onClick={handleSignOut}
-							className="m-1 px-3 py-1 rounded select-none transition-all text-white bg-red-700 lg:hover:bg-red-800"
+							className="m-1 px-1 md:px-3 py-1 rounded select-none text-xs md:text-base transition-all text-white bg-red-700 lg:hover:bg-red-800"
 						>
 							Logout
 						</button>
@@ -269,43 +252,47 @@ export default function Form() {
 					<h1 className="mx-auto mt-5 mb-1 p-1 select-none text-2xl md:text-xl">
 						STME CV Form
 					</h1>
-					<form
-						className="mx-auto mt-1 mb-10 p-3 w-11/12 md:w-9/12 flex flex-col justify-around place-items-center"
-						onSubmitCapture={handleSubmit}
-					>
-						<PersonalDetails email={sUser.email} />
-						<Academics />
-						<Skills />
-						<Extracurricular />
-						<Internship
-							internCounter={internCounter}
-							addInternship={addInternship}
-							removeInternship={removeInternship}
-						/>
-						<Project
-							projectCount={projectCount}
-							addProject={addProject}
-							removeProject={removeProject}
-						/>
-						<Leadership
-							leadCount={leadCount}
-							addLead={addLead}
-							removeLead={removeLead}
-						/>
-						<div className="m-1 p-1 w-full border-t-2 flex flex-col justify-around place-items-center ">
-							<p className="m-1 p-1 text-xs select-none italic">
-								(You've checked all the form values and are ready to submit the
-								data)
-							</p>
-							<button
-								type="submit"
-								className="mx-auto my-5 px-10 py-2 select-none transition-all duration-500 text-white bg-gray-700 lg:hover:rounded-3xl lg:hover:bg-gray-800"
-							>
-								Submit
-							</button>
-						</div>
-					</form>
-					<footer className="px-auto py-16 w-full grid place-items-center select-none text-white bg-gray-900">
+					{!hasData ?
+						<form
+							className="mx-auto mt-1 mb-10 p-3 w-11/12 md:w-9/12 flex flex-col justify-around place-items-center"
+							onSubmitCapture={handleSubmit}
+						>
+							<PersonalDetails email={sUser.email} />
+							<Academics />
+							<Skills />
+							<Extracurricular />
+							<Internship
+								internCounter={internCounter}
+								addInternship={addInternship}
+								removeInternship={removeInternship}
+							/>
+							<Project
+								projectCount={projectCount}
+								addProject={addProject}
+								removeProject={removeProject}
+							/>
+							<Leadership
+								leadCount={leadCount}
+								addLead={addLead}
+								removeLead={removeLead}
+							/>
+							<div className="m-1 p-1 w-full border-t-2 flex flex-col justify-around place-items-center ">
+								<p className="m-1 p-1 text-xs select-none italic">
+									(You've checked all the form values and are ready to submit the
+									data)
+								</p>
+								<button
+									type="submit"
+									className="mx-auto my-5 px-10 py-2 select-none transition-all duration-500 text-white bg-gray-700 lg:hover:rounded-3xl lg:hover:bg-gray-800"
+								>
+									Submit
+								</button>
+							</div>
+						</form>
+						:
+						<span className='mx-auto my-40 px-3 py-2 text-base rounded-3xl select-none text-white bg-purple-800'>You've already filled the form</span>
+					}
+					<footer className="px-auto py-16 w-full flex flex-col justify-around place-items-center select-none text-white bg-gray-900">
 						<h4 className="mx-auto my-5 px-5 py-2 text-center border animate-pulse rounded border-purple-700">
 							DEVELOPED BY
 						</h4>
@@ -318,7 +305,8 @@ export default function Form() {
 				</>
 			) : (
 				<span className="m-10 p-10">Not signed in</span>
-			)}
-		</div>
+			)
+			}
+		</div >
 	)
 }
