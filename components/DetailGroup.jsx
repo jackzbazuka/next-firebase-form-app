b3d23b675551944a847817a15c52ba81b2c60ad3
ShivS01 <shivanshusingh10@yahoo.in>
2021-07-07 16:30:23 +0530

    maxLen 45 to 70

--- components/DetailGroup.jsx
+++ components/DetailGroup.jsx
@@ -8,7 +8,7 @@ export default function DetailGroup({ detailField, count }) {
 					className="m-2 px-3 py-2 w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
 					name={`${dataField.name}${count}`}
 					placeholder={dataField.placeholder}
-					maxLength="45"
+					maxLength="70"
 					required
 				/>
 			))}
